62a55a95382552193be6181d440a481ffa280c98
Wiktor <Wiktor@DESKTOP-8DRIGKV>
2023-07-30 20:12:02 +0200

    model improved

--- training/sampleImagesMultiplier.py
+++ training/sampleImagesMultiplier.py
@@ -3,8 +3,7 @@ from keras.preprocessing.image import ImageDataGenerator, save_img
 import uuid
 import shutil
 
-number_of_copies = 3
-
+number_of_copies = 2
 
 def save_image(image, class_name):
     save_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'trainingData', 'processed', class_name)
@@ -15,9 +14,11 @@ def save_image(image, class_name):
     image_path = os.path.join(save_directory, new_filename)
     save_img(image_path, image)
 
-def save_images_from_generator(generator):
+def save_images_from_generator(generator, number_of_images_to_process):
     nb_of_images_processed = 0
     for x, y in generator:
+        if nb_of_images_processed > number_of_images_to_process:
+            break
         nb_of_images_processed += x.shape[0]
         for image_nb in range(x.shape[0]):
             # Get the corresponding class name for the current image batch
@@ -25,7 +26,19 @@ def save_images_from_generator(generator):
             class_name = [k for k, v in class_name.items() if v == y[image_nb]][0]
             save_image(x[image_nb], class_name)  
 
+
 if __name__ == '__main__':
+
+    result_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'trainingData', 'processed')
+    shutil.rmtree(result_directory, ignore_errors=True)
+    os.makedirs(result_directory, exist_ok=True)
+    data_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'trainingData', 'raw')
+    copy_images_generator = ImageDataGenerator()
+    generator = copy_images_generator.flow_from_directory(data_directory, batch_size=32, class_mode='binary', target_size=[90,160])
+    number_of_input_images = sum(len(files) for _, _, files in os.walk(data_directory))
+    save_images_from_generator(generator, number_of_input_images)
+    print(number_of_input_images)
+
     Gen = ImageDataGenerator(
         rotation_range=0,
         width_shift_range=0.1,
@@ -38,12 +51,6 @@ if __name__ == '__main__':
         preprocessing_function=None
     )
 
-
-    result_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'trainingData', 'processed')
-    shutil.rmtree(result_directory)
-    data_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'trainingData', 'raw')
-    shutil.copytree(data_directory, result_directory)
-
     for i in range(number_of_copies):
-        generator = Gen.flow_from_directory(data_directory, batch_size=10, class_mode='binary', target_size=[90,160])
-        save_images_from_generator( generator)
+        generator = Gen.flow_from_directory(data_directory, batch_size=32, class_mode='binary', target_size=[90,160])
+        save_images_from_generator( generator, number_of_input_images*number_of_copies)
