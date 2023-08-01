15427557b51b8827c1e6ca263bab4430be37c4f2
Wiktor <Wiktor@DESKTOP-8DRIGKV>
2023-08-01 17:26:05 +0200

    classifier improvements, new model

--- training/imageBackgroundChange.py
+++ training/imageBackgroundChange.py
@@ -2,6 +2,7 @@ import os
 import random
 from rembg import remove
 from PIL import Image
+import threading
 
 def load_backgrounds(backgrounds_directory):
     background_images = []
@@ -13,40 +14,41 @@ def load_backgrounds(backgrounds_directory):
     return background_images
 
 def composite_with_random_background(foreground_path, background_images, output_directory):
-    # Load the foreground image (with background to be removed)
     foreground = Image.open(foreground_path)
-
-    # Remove the background from the foreground image
-    removed_background = remove(foreground)
-
-    # Select a random background image from the cache
+    no_background = remove(foreground)
     background = random.choice(background_images)
-
-    # Make sure the background image and the removed background have the same size
     if foreground.size != background.size:
         background = background.resize(foreground.size)
+    composite = Image.alpha_composite(background.convert("RGBA"), no_background)
 
-    # Composite the foreground image with the new background
-    composite = Image.alpha_composite(background.convert("RGBA"), removed_background)
-
-    # Save the final composite image in JPEG format
     output_filename = os.path.splitext(os.path.basename(foreground_path))[0] + "_with_new_background.jpg"
     output_path = os.path.join(output_directory, output_filename)
+    composite = composite.convert("RGB")
     composite.save(output_path, format="JPEG")
 
-# Process all images from the given directory recursively
-input_directory = "F:\\Programming\\Posture\\training"
-backgrounds_directory = "F:\\Programming\\Posture\\backgrounds"
-output_directory = "F:\\Programming\\Posture\\training_with_new_backgrounds"
+input_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'trainingData', 'processed', 'class_1')
+backgrounds_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'backgrounds', 'backgrounds')
 
-if not os.path.exists(output_directory):
-    os.makedirs(output_directory)
-
-# Cache all background images
 background_images = load_backgrounds(backgrounds_directory)
 
-for root, _, files in os.walk(input_directory):
-    for file in files:
-        if file.lower().endswith((".jpg", ".jpeg", ".png")):
+def process_directory(input_directory):
+    file_paths = []
+    for root, _, files in os.walk(input_directory):
+        for file in files:
+            if file.lower().endswith((".jpg", ".jpeg", ".png"))  and str(file).find('_with_new_background') == -1:
+                foreground_path = os.path.join(root, file)
+                file_paths.append(foreground_path)
+
+    random.shuffle(file_paths)
+
+    for file in file_paths:
             foreground_path = os.path.join(root, file)
-            composite_with_random_background(foreground_path, background_images, output_directory)
+            composite_with_random_background(foreground_path, background_images, input_directory)
+
+timer_thread = threading.Thread(target=process_directory, args=[input_directory])
+timer_thread.daemon = True
+timer_thread.start()
+
+input_directory2 = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'trainingData', 'processed', 'class_0')
+
+process_directory(input_directory2)
