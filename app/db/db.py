ed11ba7cd59994062503bfa5d38c47fb059c4104
Wiktor <Wiktor@DESKTOP-8DRIGKV>
2023-08-06 23:47:05 +0200

    today correctly time displayed

--- app/db/db.py
+++ app/db/db.py
@@ -1,16 +1,37 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from db.statisticRecord import Base, StatisticRecord
+from sqlalchemy import func
+import datetime
 
 def save_statistics_to_db(valid_time, invalid_time):
-    engine = create_engine('sqlite:///bonk.db')
-    Base.metadata.create_all(engine)
-
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    session = create_session()
 
     new_object = StatisticRecord(valid_time=valid_time, invalid_time=invalid_time)
     session.add(new_object)
     session.commit()
 
     session.close()
+
+def create_session():
+    engine = create_engine('sqlite:///bonk.db')
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return session
+
+def get_today_statistics():
+        session = create_session()
+        today = datetime.date.today()
+        total_valid_time, total_invalid_time = session.query(
+            func.sum(StatisticRecord.valid_time),
+            func.sum(StatisticRecord.invalid_time)
+        ).filter(StatisticRecord.date_added == today).first()
+
+        session.close()
+
+        if total_valid_time is None or total_invalid_time is None:
+                return 0, 0
+        else:
+                return total_valid_time, total_invalid_time
--- app/db/statisticRecord.py
+++ app/db/statisticRecord.py
@@ -1,5 +1,9 @@
-from sqlalchemy import create_engine, Column, Integer
+from sqlalchemy import Date, create_engine, Column, Integer
 from sqlalchemy.ext.declarative import declarative_base
+import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import func
+import datetime
 
 Base = declarative_base()
 
@@ -8,7 +12,9 @@ class StatisticRecord(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     valid_time = Column(Integer)
     invalid_time = Column(Integer)
+    date_added = Column(Date)
 
     def __init__(self, valid_time, invalid_time):
         self.valid_time = valid_time
         self.invalid_time = invalid_time
+        self.date_added = datetime.date.today()
