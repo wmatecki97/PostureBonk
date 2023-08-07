7e61d3065f3fdc4a5bfd5902d016585cd2262b32
Wiktor <Wiktor@DESKTOP-8DRIGKV>
2023-08-08 00:23:56 +0200

    training process simplified

--- training/classifier.py
+++ training/classifier.py
@@ -8,15 +8,18 @@ import cv2
 import numpy as np
 from tensorflow.keras.callbacks import LearningRateScheduler
 
+
 def build_image_classifier(input_shape, num_classes):
     model = Sequential()
     model.add(Flatten(input_shape=input_shape))
     model.add(Dense(32, activation='relu'))
-    model.add(Dense(num_classes, activation='softmax'))  # Changed to 'softmax' activation
+    # Changed to 'softmax' activation
+    model.add(Dense(num_classes, activation='softmax'))
 
     return model
 
-input_shape = (90,160)
+
+input_shape = (90, 160)
 num_classes = 2
 learning_rate = 0.0001
 batch_size = 8
@@ -28,40 +31,48 @@ dataset = tf.data.TFRecordDataset('trainingData/data.tfrecords')
 model = build_image_classifier(input_shape, num_classes)
 
 optimizer = Adam(learning_rate=learning_rate)
-model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-# Changed loss to 'sparse_categorical_crossentropy' since labels are integers (0 or 1).
-#model = load_model(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..','app','assets', 'image_classifier_model.h5'))
+model.compile(optimizer=optimizer,
+              loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+# model = load_model(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..','app','assets', 'image_classifier_model.h5'))
 
 train_datagen = ImageDataGenerator(rescale=1.0 / 255)
 
-def custom_image_generator(generator):
+
+def black_and_white_images_generator(generator):
     for x, y in generator:
-        x = [modify_image(img) for img in x]
+        x = [convert_to_black_and_white(img) for img in x]
         x = np.array(x)
         yield x, y
 
-def modify_image(img):
+
+def convert_to_black_and_white(img):
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     modified_img = np.where(gray_img < 1, 0, 1)
     return modified_img
 
+
 # Use the custom generator
-train_generator = custom_image_generator(train_datagen.flow_from_directory(
-    os.path.join(os.path.dirname(os.path.abspath(__file__))+'/trainingData', 'processed'),
+train_generator = black_and_white_images_generator(train_datagen.flow_from_directory(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)) +
+                 '/trainingData', 'processed'),
     target_size=(90, 160),
     batch_size=batch_size,
     class_mode='binary',
     classes=['class_0', 'class_1']
 ))
 
+
 def lr_schedule(epoch):
     initial_lr = learning_rate
     new_lr = initial_lr * tf.math.exp(-0.5 * epoch)
     return new_lr
 
+
 lr_scheduler = LearningRateScheduler(lr_schedule)
 
-model.fit(train_generator, epochs=epochs, steps_per_epoch=steps_per_epoch, callbacks=[lr_scheduler])
+model.fit(train_generator, epochs=epochs,
+          steps_per_epoch=steps_per_epoch, callbacks=[lr_scheduler])
 
 # Save the model to a file
-model.save( os.path.join(os.path.dirname(os.path.abspath(__file__)),'..','app','assets','image_classifier_model.h5'))
+model.save(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+           '..', 'app', 'assets', 'image_classifier_model.h5'))
--- training/videoToImages.py
+++ training/videoToImages.py
@@ -3,100 +3,57 @@ import random
 import os
 import shutil
 from cvzone.SelfiSegmentationModule import SelfiSegmentation
-import numpy as np
 
 seg = SelfiSegmentation()
-# backgrounds_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'backgrounds', 'backgrounds')
 
-def load_backgrounds(backgrounds_directory):
-    background_images = []
-    for filename in os.listdir(backgrounds_directory):
-        if filename.lower().endswith((".jpg", ".jpeg", ".png")):
-            background_path = os.path.join(backgrounds_directory, filename)
-            background_image = cv2.imread(background_path)
-            if background_image is not None:
-                background_image = cv2.resize(background_image, (160, 90))
-                background_images.append(background_image)
-    return background_images
-
-# background_images = load_backgrounds(backgrounds_directory)
 
 def video_to_images(input_video_path, output_image_path):
     global background_images
-    # Open the video file
     video_capture = cv2.VideoCapture(input_video_path)
-    
-    # Check if the video file was opened successfully
+
     if not video_capture.isOpened():
         print("Error: Could not open video file.")
         return
-    
-    # Initialize frame count
+
     frame_count = 0
 
     while True:
-        # Read a single frame from the video
         ret, frame = video_capture.read()
-        
-        # If the frame was not read successfully, break out of the loop
+
         if not ret:
             break
-        
-        # Save the frame as an image
+
         image_path = f"{output_image_path}/frame_{os.path.basename(input_video_path)+str(frame_count)}.jpg"
         if os.path.isfile(image_path):
             break
-        
-        # background = random.choice(background_images)
-        frame = cv2.resize(frame,(160,90))
+
+        frame = cv2.resize(frame, (160, 90))
         vid_rmbg = seg.removeBG(frame, threshold=0.6)
         cv2.imwrite(image_path, vid_rmbg)
-        
-        # Increment frame count
+
         frame_count += 1
-    
-    # Release the video capture object
+
     video_capture.release()
 
+
 def remove_files_with_copy(folder_path):
-        os.makedirs(folder_path, exist_ok=True)
-        # Get the list of all files in the folder
-        files = os.listdir(folder_path)
+    os.makedirs(folder_path, exist_ok=True)
+    files = os.listdir(folder_path)
 
-        # Iterate through the files and remove those containing '_copy' in their names
-        for file_name in files:
-            if '_copy' in file_name:
-                file_path = os.path.join(folder_path, file_name)
-                os.remove(file_path)
+    for file_name in files:
+        if '_copy' in file_name:
+            file_path = os.path.join(folder_path, file_name)
+            os.remove(file_path)
 
-if __name__ == "__main__":
 
-   # Folder paths
-    correct_video_folder =os.path.join( os.path.dirname(os.path.abspath(__file__)),"trainingVideos","correct")
-    print(correct_video_folder)
-    invalid_video_folder =os.path.join(  os.path.dirname(os.path.abspath(__file__)),"trainingVideos","invalid")
-    
-    # Output folders for the respective classes
-    class_0_output_folder = os.path.join( os.path.dirname(os.path.abspath(__file__)),"trainingData", "raw","class_0")
-    class_1_output_folder = os.path.join( os.path.dirname(os.path.abspath(__file__)),"trainingData", "raw","class_1")
-    
-    remove_files_with_copy(class_0_output_folder)
-    remove_files_with_copy(class_1_output_folder)
-
-    # Process the 'correct' video folder
-    correct_files = os.listdir(correct_video_folder)
-    for file in correct_files:
+def process_directory(directory, output_directory, files):
+    for file in files:
         if file.endswith(".mp4"):
-            input_video_path = os.path.join(correct_video_folder, file)
-            video_to_images(input_video_path, class_0_output_folder)
-    
-    # Process the 'invalid' video folder
-    invalid_files = os.listdir(invalid_video_folder)
-    for file in invalid_files:
-        if file.endswith(".mp4"):
-            input_video_path = os.path.join(invalid_video_folder, file)
-            video_to_images(input_video_path, class_1_output_folder)
+            input_video_path = os.path.join(directory, file)
+            video_to_images(input_video_path, output_directory)
 
+
+def equalize_number_of_samples_for_classes(class_0_output_folder, class_1_output_folder):
     correct_images = os.listdir(class_0_output_folder)
     invalid_images = os.listdir(class_1_output_folder)
 
@@ -107,7 +64,7 @@ if __name__ == "__main__":
         images_to_duplicate = invalid_images
         num_to_duplicate = len(correct_images) - len(invalid_images)
         source_folder = class_1_output_folder
-    
+
     images_to_duplicate = random.sample(images_to_duplicate, num_to_duplicate)
     for image_name in images_to_duplicate:
         image_path = os.path.join(source_folder, image_name)
@@ -116,6 +73,30 @@ if __name__ == "__main__":
         new_image_path = os.path.join(source_folder, new_image_name)
 
         shutil.copy(image_path, new_image_path)
-    
 
-    
+
+if __name__ == "__main__":
+
+    correct_video_folder = os.path.join(os.path.dirname(
+        os.path.abspath(__file__)), "trainingVideos", "correct")
+    print(correct_video_folder)
+    invalid_video_folder = os.path.join(os.path.dirname(
+        os.path.abspath(__file__)), "trainingVideos", "invalid")
+
+    class_0_output_folder = os.path.join(os.path.dirname(
+        os.path.abspath(__file__)), "trainingData", "raw", "class_0")
+    class_1_output_folder = os.path.join(os.path.dirname(
+        os.path.abspath(__file__)), "trainingData", "raw", "class_1")
+
+    remove_files_with_copy(class_0_output_folder)
+    remove_files_with_copy(class_1_output_folder)
+
+    correct_files = os.listdir(correct_video_folder)
+    invalid_files = os.listdir(invalid_video_folder)
+    process_directory(correct_video_folder,
+                      class_0_output_folder, correct_files)
+    process_directory(invalid_video_folder,
+                      class_1_output_folder, invalid_files)
+
+    equalize_number_of_samples_for_classes(
+        class_0_output_folder, class_1_output_folder)
